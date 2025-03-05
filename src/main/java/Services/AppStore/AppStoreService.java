4846aee2af9e90ce86c194b1ebfe7a33315499a3
Aryan171 <62366692+Aryan171@users.noreply.github.com>
2025-03-05 13:48:24 +0530

    wrote the AppStoreService which implements iEmployeeService interface

--- src/main/java/Services/AppStore/AppStoreService.java
+++ src/main/java/Services/AppStore/AppStoreService.java
@@ -1,28 +1,43 @@
 package Services.AppStore;
 
 import Model.AppStore;
+import Repository.AppStoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class AppStoreService implements iAppStoreInterface{
+
+    @Autowired
+    private AppStoreRepository appStoreRepository;
 
     @Override
     public AppStore addProduct(AppStore product) {
-        return null;
+        return appStoreRepository.save(product);
     }
 
     @Override
     public AppStore getProductById(Integer id) {
-        return null;
+        Optional<AppStore> product = appStoreRepository.findById(id);
+        return product.orElse(null);
     }
 
     @Override
     public boolean deleteProductById(Integer id) {
-        return false;
+        if (appStoreRepository.existsById(id)) {
+            appStoreRepository.deleteById(id);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
     public List<AppStore> getAllProducts() {
-        return List.of();
+        return appStoreRepository.findAll();
     }
 }
