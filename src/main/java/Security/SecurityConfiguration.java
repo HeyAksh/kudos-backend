ac968b64752573d4673248c7b16ff027af7ea887
Aryan171 <62366692+Aryan171@users.noreply.github.com>
2025-03-05 21:24:47 +0530

    set ALWAYS as the session management policy and removed the code which disabled csrf protection

--- src/main/java/Security/SecurityConfiguration.java
+++ src/main/java/Security/SecurityConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/employee/**").authenticated()
                         .requestMatchers("/app-store/**").permitAll()
                         .requestMatchers("/event/**").permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
-        return http.build();
+                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .build();
     }
 }
