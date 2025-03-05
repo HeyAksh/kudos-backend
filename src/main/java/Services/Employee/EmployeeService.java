53147fd771051fac0cc350a73ce0e41f4b33d4db
Aryan171 <62366692+Aryan171@users.noreply.github.com>
2025-03-05 13:42:46 +0530

    wrote the EmployeeService which implements iEmployeeService interface

--- src/main/java/Services/Employee/EmployeeService.java
+++ src/main/java/Services/Employee/EmployeeService.java
@@ -1,32 +1,53 @@
 package Services.Employee;
 
 import Model.Employee;
+import Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class EmployeeService implements iEmployeeService{
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Override
     public Employee addEmployee(Employee employee) {
-        return null;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public Employee getEmployeeById(Integer id) {
-        return null;
+        Optional<Employee> employee = employeeRepository.findById(id);
+        return employee.orElse(null);
     }
 
     @Override
     public boolean deleteEmployeeById(Integer id) {
-        return false;
+        if(employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return List.of();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee updateEmployee(Integer id, Employee employee) {
-        return null;
+        if(employeeRepository.existsById(id)) {
+            employee.setEmployeeId(id);
+            return employeeRepository.save(employee);
+        }
+        else {
+            return null;
+        }
     }
 }
