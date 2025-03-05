f262464dfa8e3175edfb478d9d69e773dcb63c9c
HeyAksh <aksh07.me@gmail.com>
2025-03-05 20:58:04 +0530

    Resolved Database Connection Issue : Component Scanning

--- src/main/java/com/backend/kudos/KudosApplication.java
+++ src/main/java/com/backend/kudos/KudosApplication.java
@@ -1,9 +1,12 @@
 package com.backend.kudos;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "Model")
 public class KudosApplication {
 
 	public static void main(String[] args) {
